19960b57a1ac04a2a674af845e6ca369b71ff64d
ak <14f143@gmail.com>
2022-10-23 23:04:25 +0300

    added alias for size_t and fixed a mistake in for loop

--- allocator.cpp
+++ allocator.cpp
@@ -20,52 +20,59 @@ class Alloc
 {
     public:
         using value_type = T;
+        using size_type = size_t;
 
         Alloc();
-        Alloc(const size_t&);
+        Alloc(const size_type&);
 
         template <typename U>
         Alloc(const Alloc<U>&) throw();
 
-        T* allocate(const size_t);
+        T* allocate(const size_type&);
         
-        void deallocate(T *p, size_t);
+        void deallocate(T*, size_type);
+
+        size_type max_size() const;
 
     private:
-        size_t _preallocated_slots;
-        bool _reserved;
+        size_type _preallocated_slots;
 };
 
 template <typename T>
 Alloc<T>::Alloc() {}
 
 template <typename T>
-Alloc<T>::Alloc(const size_t &elements) : _preallocated_slots{elements}, _reserved{false} {}
+Alloc<T>::Alloc(const Alloc::size_type &elements) : _preallocated_slots{elements} {}
 
 template <typename T>
 template <typename U>
 Alloc<T>::Alloc(const Alloc<U> &alloc) throw() {}
 
 template <typename T>
-T* Alloc<T>::allocate(const size_t elements)
+T* Alloc<T>::allocate(const Alloc::size_type &elements)
 {
-    if (!_reserved)
-    {
-        auto *p = std::malloc(_preallocated_slots * sizeof(T));
-        if (!p)
-            throw std::bad_alloc();
-        _reserved = true;
-        return reinterpret_cast<T*>(p);
-    }
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    auto *p = std::malloc(elements * sizeof(T));
+    if (!p) throw std::bad_alloc();
+
+    std::cout << "allocated " << elements << " ellements at " << &p << std::endl;
+
+    return reinterpret_cast<T*>(p);
 }
 
 template <typename T>
-void Alloc<T>::deallocate(T *p, size_t elements)
+void Alloc<T>::deallocate(T *p, Alloc::size_type elements)
 {
     std::free(p);
 }
 
-template <class T, size_t S1, class U, size_t S2>
+template <typename T>
+typename Alloc<T>::size_type Alloc<T>::max_size() const
+{
+    return _preallocated_slots;
+}
+
+template <class T, class U>
 constexpr bool operator==(const Alloc<T>&, const Alloc<U>&) noexcept
 {
     return false;
@@ -79,15 +86,12 @@ constexpr bool operator!=(const Alloc<T>&, const Alloc<U>&) noexcept
 
 int main()
 {
-    Alloc<int> alloc(10);
-    std::vector<int, Alloc<int>> v(10, alloc);
-    //for (auto i = 0; i < v.size(); ++i)
-    //    v.push_back(i);
-
-    /*
-    for (auto i = 0; i < v.size(); ++i)
-        std::cout << v[i] << ' ' << std::endl;
-        */
-
+    Alloc<int> alloc(3);
+    std::vector<int, Alloc<int>> v(alloc);
+    for (auto i = 0; i < 3; ++i)
+    {
+        v.push_back(i);
+    }
+        
     return 0;
 }
