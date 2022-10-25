c3a614d660f0c438b5ebf5e26a46534b9c4aac5f
ak <14f143@gmail.com>
2022-10-26 00:59:30 +0300

    added pointer's arithmetic for memory allocation

--- allocator.cpp
+++ allocator.cpp
@@ -31,6 +31,7 @@ class Alloc
         };
 
         Alloc();
+        ~Alloc();
 
         template <typename U>
         Alloc(const Alloc<S, U>&) throw();
@@ -42,18 +43,27 @@ class Alloc
         size_type max_size() const;
 
     private:
-        size_type _reserved_slots;
-        size_type _used;
-        void* _memory_begin;
+        T *_memory_begin;
+        T *_memory_end;
+        T *_memory_used;
 };
 
 template <int S, typename T>
 Alloc<S, T>::Alloc() :
-    _reserved_slots{S},
-    _used{0},
-    _memory_begin{nullptr}
+    _memory_begin{nullptr},
+    _memory_end{nullptr},
+    _memory_used{nullptr}
 {
 }
+
+template <int S, typename T>
+Alloc<S, T>::~Alloc()
+{
+    std::free(_memory_begin);
+    _memory_used = nullptr;
+    _memory_end = nullptr;
+}
+
 
 template <int S, typename T>
 template <typename U>
@@ -62,32 +72,39 @@ Alloc<S, T>::Alloc(const Alloc<S, U> &) throw() {}
 template <int S, typename T>
 T* Alloc<S, T>::allocate(const Alloc::size_type &elements)
 {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
     if (_memory_begin == nullptr)
     {
-        _memory_begin = std::malloc(sizeof(T) * _reserved_slots);
-        _used = elements;
+        _memory_begin = reinterpret_cast<T*>(std::malloc(sizeof(T) * S));
+        _memory_used = _memory_begin;
+        _memory_end = _memory_begin + S * sizeof(T);
+
+        std::cout << "_memory_begin at " << &_memory_begin << std::endl;
+        std::cout << "_memory_end at " << &_memory_end << std::endl;
     }
-    else if (_used + elements < _reserved_slots)
+    else if (_memory_used + elements * sizeof(T) < _memory_end)
     {
-        _used += elements;
+        _memory_used += elements * sizeof(T);
     }
     else
     {
         throw std::bad_alloc();
     }
-    return reinterpret_cast<T*>(_memory_begin);
+
+    std::cout << "_memory_used at " << &_memory_used << std::endl;
+    return _memory_used;
 }
 
 template <int S, typename T>
-void Alloc<S, T>::deallocate(T *p, Alloc::size_type)
+void Alloc<S, T>::deallocate(T*, Alloc::size_type)
 {
-    std::free(p);
 }
 
 template <int S, typename T>
 typename Alloc<S, T>::size_type Alloc<S, T>::max_size() const
 {
-    return _reserved_slots;
+    return S;
 }
 
 template <int S, class T, class U>
@@ -106,9 +123,11 @@ int main()
 {
     std::map<int, int, std::less<int>, Alloc<4, std::pair<const int, int>>> m;
     std::cout << "start to fill" << std::endl;
-    m[1] = 1;
-    m[2] = 2;
-    m[3] = 3;
+    m[1] = Factorial<1>::value;
+    m[2] = Factorial<2>::value;
+    m[3] = Factorial<3>::value;
+    m[4] = Factorial<4>::value;
+    //m[5] = Factorial<5>::value; Allocator throw the error here
         
     return 0;
 }
