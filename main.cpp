a7220d12222160bfe868e851e1453b2346e92337
ak <14f143@gmail.com>
2022-10-30 21:24:32 +0300

    done a few tasks related to custom container in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <iostream>
 #include "allocator.hpp"
+#include "list.hpp"
 
 int factorial(int i)
 {
@@ -18,6 +19,22 @@ int main()
     std::map<int, int, std::less<int>, Alloc<10, std::pair<const int, int>>> m2;
     for (int i = 0; i < 10; ++i)
         m2[i] = factorial(i);
+
+    for (const auto& [key, value] : m2)
+    {
+        std::cout << key << " " << value << std::endl;
+    }
         
+    List<int> l1;
+    for (int i = 0; i < 10; ++i)
+        l1.push_back(i);
+
+    List<int, Alloc<10, int>> l2;
+    for (int i = 0; i < 10; ++i)
+        l2.push_back(i);
+
+    for(auto it = l2.begin(); it != l2.end(); ++it)
+        std::cout << *it << std::endl;
+
     return 0;
 }
